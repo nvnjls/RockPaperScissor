cfd40d6370a6f18fde18bf3ab3818a38af74953a
nvnjls <nvnjls@gmail.com>
2017-05-22 16:43:18 +0530

    Project commit

---
+++ Assets/NController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NController : MonoBehaviour {
+
+    Animator a;
+    int x;
+    int maxShots;
+    // Use this for initialization
+    void Start()
+    {
+        maxShots = 2;
+        a = this.gameObject.GetComponent<Animator>();
+        x = maxShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            x = maxShots;
+        }
+
+
+
+        if (Input.GetKeyDown(KeyCode.W) && x>0)
+        {
+            a.SetTrigger("HP");
+            x--;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && x > 0)
+        {
+            a.SetTrigger("HK");
+            x--;
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && x > 0)
+        {
+            a.SetTrigger("S");
+            x--;
+        }
+
+    }
+}
---
+++ Assets/PController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PController : MonoBehaviour {
+    Animator a;
+    int x;
+    int maxShots;
+    // Use this for initialization
+    void Start()
+    {
+        maxShots = 2;
+        a = this.gameObject.GetComponent<Animator>();
+        x = maxShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            x = maxShots;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Keypad9) && x > 0)
+        {
+            a.SetTrigger("HP");
+            x--;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad6) && x > 0)
+        {
+            a.SetTrigger("HK");
+            x--;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3) && x > 0)
+        {
+            a.SetTrigger("S");
+            x--;
+        }
+
+    }
+}
